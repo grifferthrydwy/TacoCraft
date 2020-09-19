6532443ff31baf30d59f5a73dfbda49e745e8826
ffrann <45723631+franiscoder@users.noreply.github.com>
2020-09-18 21:05:47 -0400

    Added Corn And Corn Kernels to compostable items

---
+++ src/main/java/io/github/franiscoder/tacocraft/mixin/MixinComposterBlock.java
@@ -0,0 +1,18 @@
+package io.github.franiscoder.tacocraft.mixin;
+
+import io.github.franiscoder.tacocraft.init.ModItems;
+import net.minecraft.block.ComposterBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ComposterBlock.class)
+public class MixinComposterBlock {
+
+    @Inject(method = "registerDefaultCompostableItems", at = @At(value = "TAIL"))
+    private static void registerSeed(CallbackInfo info) {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CORN_SEED, 0.3F);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CORN, 0.65F);
+    }
+}
